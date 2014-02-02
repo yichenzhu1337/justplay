575af73c53669438fd3ab2a86750339fbf6a8b7a
Jack Yiu <jackinyiu@gmail.com>
2014-02-02 17:42:04 -0500

    [front-end] Sidebar collapsible to icon-sidebar

--- js/playground.js
+++ js/playground.js
@@ -1,4 +1,21 @@
-$(function(){
-	$('#side-menu').metisMenu();
-});
+$(document).ready(function(){
+	$(function(){
+		$('#side-menu').metisMenu();
+	});
 
+	$("#Sidebar-menu-button").click(function(){
+		
+		$(".sidebar-right-content").toggle();
+		var width=$(".navbar-fixed-side").width();
+		var shrink=(width>50)?true:false;
+		//alert(width);
+		if (shrink){
+			//alert("Shrinkit!");	
+			$(".navbar-fixed-side").css({"width":"50px"});
+		} else {
+			//alert("Expandit!");	
+			$(".navbar-fixed-side").css({"width":"200px"});
+		}
+		
+	})	
+});

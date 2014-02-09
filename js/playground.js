d9ef55ac5c0517675a9800ce657a20aed51ea2f3
Jack Yiu <jackinyiu@gmail.com>
2014-02-09 01:35:37 -0500

    [Front-end] Downloaded pre-compiled Bootstrap, enables modification of bootstrap LESS files. Modified sidebar so that it is automatically centered (Uses heavy JS to detect window middle and adjust top padding).

--- js/playground.js
+++ js/playground.js
@@ -2,8 +2,31 @@
 var ICON_SIDE_BAR_WIDTH= 45;
 var SIDE_BAR_WIDTH=200;
 var MAIN_CONTENT_WRAPPER_PADDING=15;
+var SIDE_BAR_LIST_ITEM_HEIGHT = 40;
 
 $(document).ready(function(){
+	//alert("begin");
+
+	/* Side Bar */
+	// Setup Vertical Distribution
+	var sideMenuChildren = $('#side-menu').children().length; // If there's a menu icon do -1.
+
+	window.onresize = function(){
+		if (sideMenuChildren>0)
+		{
+			var halfWindowHeight = $(window).height()/2;
+			var amount = 0;
+			if (sideMenuChildren%2 == 0)
+			{
+				amount = parseInt(halfWindowHeight)-parseInt(sideMenuChildren/2*SIDE_BAR_LIST_ITEM_HEIGHT);
+			} else {
+				amount = parseInt(halfWindowHeight)-parseInt((sideMenuChildren-1)/2*SIDE_BAR_LIST_ITEM_HEIGHT)-parseInt(SIDE_BAR_LIST_ITEM_HEIGHT/2);
+			}
+			$('#side-menu').css({"padding-top":amount+"px"});
+		}
+		
+	}
+	window.onresize();
 	$(function(){
 		$('#side-menu').metisMenu();
 	});
@@ -23,5 +46,7 @@ $(document).ready(function(){
 		}
 		$('.main-content-wrapper').css({"padding-left":parseInt($(".navbar-fixed-side").width())+parseInt(MAIN_CONTENT_WRAPPER_PADDING)+"px"});
 		
-	})	
+	})
+
+
 });

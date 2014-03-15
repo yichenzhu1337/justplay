3a72eb1d3f6126b872458d369df62dfb0bac9f05
omfgitsjack <jackinyiu@gmail.com>
2014-03-14 23:40:29 -0400

    [Front-end] Left Right arrow

--- js/playground.js
+++ js/playground.js
@@ -139,12 +139,42 @@ $(document).ready(function(){
                                         }
                             });
     });
-
+    flip("asdf","asd","asd","asd","asd");
     // Tooltip setup
     $(function() {
         $('.friend-mouseover').tooltip({placement: 'right'});
         $('.star-mouseover').tooltip({placement: 'top'});
     });
 
+    var flip = flippant.flip;
+    var event = require('./event');
 
+document.addEventListener('click', function(e) {
+  if (e.target.classList.contains('btnflip')) {
+    e.preventDefault()
+    var flipper = e.target.parentNode.parentNode
+    var back
+    var input = '<p><input type="text" value="' + flipper.querySelector('h2').innerText + '"></p>'
+    var textarea = '<textarea style="width:100%; max-width:32em; height:12em;">' + flipper.querySelector('p').innerText + '</textarea><br><button class="btn">Update</button>'
+
+    if (e.target.classList.contains('card')) {
+      back = flip(flipper, "<p>It's a card!</p>" + input + textarea)
+    } else {
+      back = flip(flipper, "<p>It's a modal!</p>" + input + textarea, 'modal')
+    }
+
+    back.addEventListener('click', function(e) {
+      if (e.target.classList.contains('btn')) {
+        flipper.querySelector('h2').innerText = back.querySelector('input').value
+        flipper.querySelector('p').innerText = back.querySelector('textarea').value
+        event.trigger(back, 'close')
+      }
+    })
+
+  }
+});
+
+    $("#hijoin").click(function(){
+        $(this).hide();
+      });
 });

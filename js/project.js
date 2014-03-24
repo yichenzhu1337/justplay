b48d75eef220bbdc805deef1199ca3fb8552dedf
omfgitsjack <jackinyiu@gmail.com>
2014-03-23 22:27:34 -0400

    [UI + Frontend] Modified UI of mouseover of a sort item. Also added angular js to correctly reflect the way in which we're sorting.

--- js/project.js
+++ js/project.js
@@ -1,4 +1,30 @@
-function SortController($scope){
-	$scope.currentstrategy = 'Earliest';
-	$scope.allstrategies = ['Earliest']
-}
+var project = angular.module('project', [])
+.factory('strategyFactory', function ()	{
+		var factory = {};
+		var strategies = [
+			'Earliest', 'Latest',
+			'Most Players', 'Least Players',
+			'Most Friends', 'Least Friends'
+		];
+		factory.getStrategies = function () {
+			return strategies;
+		};
+		return factory;
+});
+
+var controllers = {};
+
+controllers.strategyController = function($scope, strategyFactory) {
+	$scope.strategies = [];
+	init();
+	function init() {
+		$scope.strategies = strategyFactory.getStrategies();
+	};
+	$scope.currentstrategy = $scope.strategies[0];
+	$scope.setValue = function(strategy){
+		$scope.currentstrategy = strategy;
+	}
+};
+
+project.controller(controllers);
+

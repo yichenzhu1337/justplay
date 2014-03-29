25433b16f38d03d165ac7ae26b46d77afa3d9514
omfgitsjack <jackinyiu@gmail.com>
2014-03-29 14:58:29 -0400

    [Front-end] Made Cards Dynamic

--- app/js/project.js
+++ app/js/project.js
@@ -1,4 +1,4 @@
-var project = angular.module('project', [])
+var project = angular.module('project', ['angularMoment'])
 .factory('strategyFactory', function ()	{
 		var factory = {};
 		var strategies = [
@@ -26,7 +26,49 @@ var project = angular.module('project', [])
 		return sports;
 	};
 	return factory;
+})
+.factory('cardFactory', function() {
+	var factory = {};
+	// Sports should be arranged in 5s
+	var cards = [
+		{
+			enddate: window.moment().add({months: 1, hours: 2}), 
+			startdate: window.moment().add({months: 1}), 
+			skill: 3,
+			activity: 'Basketball',
+			location: 'Gym',
+			friends: 0,
+			nonfriends: 1,
+			capacity: 6,
+			minimumrequired: 2,
+		},
+		{
+			enddate: window.moment().add({months: 0, hours: 4}), startdate: window.moment().add({months: 0}),
+			skill: 2,
+			activity: 'Tennis',
+			location: 'The Valley',
+			friends: 2,
+			nonfriends: 4,
+			capacity: 6,
+			minimumrequired: 3,
+		},
+		{
+			enddate: window.moment().add({days: 1, hours: 4}), startdate: window.moment().add({days:1, hours:2}),
+			skill: 1,
+			activity: 'Badminton',
+			location: 'Gym',
+			friends: 1,
+			nonfriends: 2,
+			capacity: 6,
+			minimumrequired: 4,
+		}
+	];
+	factory.getCards = function () {
+		return cards;
+	};
+	return factory;
 });
+
 
 var controllers = {};
 
@@ -56,9 +98,54 @@ controllers.sportController = function($scope, sportFactory) {
 controllers.dateController = function($scope) {
 	init();
 	function init() {
-		$scope.selectedDate = window.moment().format("dddd, MMM Do");
+		$scope.selectedDate = window.moment();
 	};
 };
+
+controllers.cardsController = function($scope, cardFactory) {
+	init();
+	function init() {
+		$scope.cards = cardFactory.getCards();
+	};
+};
+
+controllers.cardController = function($scope) {
+	$scope.totalparticipants;
+
+	$scope.progressbar;
+	$scope.peopleneeded;
+	$scope.neededorfree;
+	$scope.stars = [];
+	$scope.skillDescription;
+	init();
+	function init() {
+		$scope.totalparticipants = $scope.card.friends+$scope.card.nonfriends;
+		// Determine People needed, description to show and color of progress bar
+		if ($scope.totalparticipants >= $scope.card.minimumrequired) {
+			$scope.progressbar = 'success';
+			$scope.neededorfree = 'SLOTS FREE';
+			$scope.peopleneeded = $scope.card.capacity - $scope.totalparticipants;
+		} else {
+			$scope.progressbar = 'warning';
+			$scope.neededorfree = 'NEEDED';
+			$scope.peopleneeded = $scope.card.minimumrequired - $scope.totalparticipants;
+		}
+		// Determine Skill Description
+		if ($scope.card.skill == 1) {
+			$scope.skillDescription = "Beginner";
+		} else if ($scope.card.skill == 2) {
+			$scope.skillDescription = "Intermediate";
+		} else if ($scope.card.skill == 3) {
+			$scope.skillDescription = "Advanced";
+		}
+		// Determine how many stars to dish out
+		for (var i = 0; i < $scope.card.skill; i++) {
+			$scope.stars.push(i);
+		}
+	};
+
+
+}
 
 project.controller(controllers);
 

b710aa063b5e365fd5b90fffa55d24521babe9c1
omfgitsjack <jackinyiu@gmail.com>
2014-05-19 21:01:12 -0400

    [Front-end] Accidentally deleted code that enforces one popover at a time rule

--- app/js/non-angular/popovers.js
+++ app/js/non-angular/popovers.js
@@ -22,4 +22,15 @@ $(document).ready(function(){
 		}
 	});
 
+	// Ensures only one pop over can be open at a time.
+	$(':not(#anything)').on('click', function (e) {
+    $('#activity-search-bar, #get-notified, #suggest-activity').each(function () {
+        //the 'is' for buttons that trigger popups
+        //the 'has' for icons and other elements within a button that triggers a popup
+        if (!$(this).is(e.target) && $(this).has(e.target).length === 0 && $('.popover').has(e.target).length === 0) {
+            $(this).popover('hide');
+            return;
+        }
+    });
+	});
 });

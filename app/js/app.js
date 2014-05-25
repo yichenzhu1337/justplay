9f12a32c01d2040d60dcbef26f311c676db2dfbf
omfgitsjack <jackinyiu@gmail.com>
2014-05-24 20:38:55 -0400

    [Front-end] Made the login/signups into directives.

--- app/js/app.js
+++ app/js/app.js
@@ -21,29 +21,70 @@ app.config(['$stateProvider', '$urlRouterProvider', function($stateProvider, $ur
 var controllers = {};
 
 controllers.loginCtrl = function($scope, $state){
+	var masterLoginCredentials = {};
 	$scope.registeredEmail = true;
 
 	$scope.submit = function(isValid) {
+		$scope.submitted = true;
 		if (isValid) {
+			masterLoginCredentials = angular.copy($scope.login);
+			console.log(masterLoginCredentials);
 			alert('Valid Login');
-			$state.go('activitylist');
+			$scope.registeredEmail = true;
+			//$state.go('activitylist');
 		} else {
+			console.log(masterLoginCredentials);
 			alert('invalid Login');
+			$scope.registeredEmail = false;
 		}
 	};
 };
 
 controllers.signupCtrl = function($scope) {
 	$scope.submit = function(isValid){
+		
 		if (isValid) {
 			alert('amazing form yo');
+			$scope.registeredEmail = true;
 		} else {
 			alert('invalid form yo');
+			$scope.registeredEmail = false;
 		}
 	}
 }
 
-var directives = {};
-
-
 app.controller(controllers);
+
+app.directive("loginform", function(){
+	// Runs during compile
+	return {
+		// {} = isolate, true = child, false/undefined = no change
+		controller: controllers.loginCtrl,
+		restrict: 'E', // E = Element, A = Attribute, C = Class, M = Comment
+		// template: 'hi',
+		templateUrl: 'views/login/form.html',
+		// replace: true,
+		// transclude: true
+		// compile: function(tElement, tAttrs, function transclude(function(scope, cloneLinkingFn){ return function linking(scope, elm, attrs){}})),
+/*		link: function($scope, iElm, iAttrs, controller) {
+			
+		}*/
+	};
+});
+
+app.directive("signupform", function(){
+	// Runs during compile
+	return {
+		// {} = isolate, true = child, false/undefined = no change
+		controller: controllers.signupCtrl,
+		restrict: 'E', // E = Element, A = Attribute, C = Class, M = Comment
+		// template: 'hi',
+		templateUrl: 'views/signup/form.html',
+		// replace: true,
+		//transclude: true
+		// compile: function(tElement, tAttrs, function transclude(function(scope, cloneLinkingFn){ return function linking(scope, elm, attrs){}})),
+/*		link: function($scope, iElm, iAttrs, controller) {
+			
+		}*/
+	};
+});

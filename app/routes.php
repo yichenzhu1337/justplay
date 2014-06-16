ff38d68969ef7b4b0c418927c91d9d1a94163e4f
omfgitsjack <jackinyiu@gmail.com>
2014-06-15 20:10:37 -0400

    [Frond-end] [Back-end] Got the CSRF Token Working

--- app/routes.php
+++ app/routes.php
@@ -24,13 +24,24 @@ Route::get('login', 'HomeController@getLogin');
  *	url/api/register	
  */
 Route::group(array('prefix' => 'api'), function(){
-	Route::post('login', 'HomeController@postLogin');
-	Route::post('register', 'HomeController@postRegister');	
+
+	/*
+		csrf
+
+	*/
+	Route::group(array('before' => 'json_csrf'), function(){
+
+		Route::post('login', 'HomeController@postLogin');
+		Route::post('register', 'HomeController@postRegister');	
+
+	});
+
+	Route::group(array('before' => 'auth'), function(){
+		Route::get('admin', 'AdminController@index'); //Route::resource('admin', 'AdminController');
+		Route::get('logout', 'HomeController@logout');
+
+	});
+
 });
 //////////////////////////////////////////////////////////
 
-Route::group(array('before' => 'auth'), function(){
-	Route::get('admin', 'AdminController@index');
-	Route::get('logout', 'HomeController@logout');
-
-});

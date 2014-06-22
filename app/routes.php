52374753ff53f4595431d593840fd7dee57c32ad
yichen <yichenzhu1337@gmail.com>
2014-06-21 23:04:18 -0400

    edited some routing and renamed HomeController to AuthenticationController

--- app/routes.php
+++ app/routes.php
@@ -5,9 +5,6 @@ Route::get('/', function()
 	return View::make("index");
 });
 
-Route::get('register', 'HomeController@getRegister');
-Route::get('login', 'HomeController@getLogin');
-
 /* 	Use:
  *	url/api/login
  *	url/api/register	
@@ -16,14 +13,17 @@ Route::group(array('prefix' => 'api'), function(){
 
 	Route::group(array('before' => 'json_csrf'), function(){
 
-		Route::post('login', 'HomeController@postLogin');
-		Route::post('register', 'HomeController@postRegister');	
+		Route::post('login', 'AuthenticationController@postLogin');
+		Route::post('register', 'AuthenticationController@postRegister');	
 
-	});
+		Route::resource('profile', 'UserProfilesController');
+		Route::resource('activity', 'ActivitiesController');
 
-	Route::group(array('before' => 'auth'), function(){
-		Route::get('admin', 'AdminController@index'); //Route::resource('admin', 'AdminController');
-		Route::get('logout', 'HomeController@logout');
+		Route::group(array('before' => 'auth'), function(){
+			Route::get('admin', 'AdminController@index'); //Route::resource('admin', 'AdminController');
+			Route::get('logout', 'HomeController@logout');
+
+		});
 
 	});
 

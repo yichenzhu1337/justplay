b571c98a7329a1c9d26eca0e4c9cf42e0364b9aa
omfgitsjack <jackinyiu@gmail.com>
2014-07-03 01:51:46 -0400

    [Front-end] Updated User Module, can now get a List of Mock Data using UserSvc.getList()

--- public/app/modules/users/userModule.js
+++ public/app/modules/users/userModule.js
@@ -18,7 +18,17 @@ factories.User = function() {
 	return User;
 };
 
-factories.UserSvc = ['$q', function($q){
+factories.UserSvc = ['$q', '$timeout', 'User', function($q, $timeout, User){
+
+	var MockUserData = 
+	[
+		{name: 'Jack Yiu', id: 2, isFriends:true, email: 'sampleEmail@email.com'},
+		{name: 'Yi Chen Zhu', id: 3, isFriends:false, email: 'sampleEmail@email.com'},
+		{name: 'Jason Zheng', id: 4, isFriends:false, email: 'sampleEmail@email.com'},
+		{name: 'Roger Ganesh', id: 5, isFriends: true, email: 'sampleEmail@email.com'},
+		{name: 'Suzanne Lim', id: 6, isFriends: true, email: 'sampleEmail@email.com'},
+		{name: 'Someoneelse', id: 7, isFriends: false, email: 'sampleEmail@email.com'},																
+	];
 
 	var APIInterceptor = function (data) {
 
@@ -30,17 +40,27 @@ factories.UserSvc = ['$q', function($q){
 		}
 	}
 
-	var search = function(name) {
-		if (angular.isUndefined(name)) {
-			var deferred = $q.defer();
+	var buildUserCollection = function(JSONObj) {
+		var array = [];
+		angular.forEach(JSONObj, function(val, key) {
+			this.push(User.build(val));
+		}, array);
+		return array;
+	}
 
-			var res = API.get("");
+	var search = function(name) {
+		if (name === "") {
+			var deferred = $q.defer();
+			var data = MockUserData;
+/*			var res = API.get("");
 			res.then(
 				function(data){
 					// Process the API results with the APIInterceptor
 
 					deferred.resolve();
-				});
+				});*/
+
+			$timeout(deferred.resolve(data),2000);			
 
 			return deferred.promise;
 		} else {
@@ -50,15 +70,16 @@ factories.UserSvc = ['$q', function($q){
 
 	return {
 		ListAll: function() {
-			var deferred = $q.defer();
+			
 
-			var searchRes = search("");
-			searchRes.then(
-				function() {
+			var searchRes = search("").then(
+				function(data) {
+					var buildData = buildUserCollection(data);
+					console.log(buildData);
+					return buildData;
+				});
 
-				})
-
-			return deferred.promise;
+			return searchRes;
 		}
 	}
 }]

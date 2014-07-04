6dbf56b9139436d61c26ea6254a2e5ba8d74b3f9
yichen <yichenzhu1337@gmail.com>
2014-07-04 19:07:36 -0400

    added resouceful routes

--- app/routes.php
+++ app/routes.php
@@ -27,6 +27,9 @@ Route::group(array('prefix' => 'api'), function(){
 
 	Route::resource('profile', 'UserProfilesController');
 	Route::resource('activity', 'ActivitiesController');
+	Route::resource('comment', 'UserProfilesController');
+	Route::resource('friends', 'UserProfilesController');
+	Route::resource('notification', 'UserProfilesController');
 });
 
 

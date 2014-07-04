1f873a2d7871dd60719129d3f98b2cc335881880
yichen <yichenzhu1337@gmail.com>
2014-07-04 18:00:27 -0400

    deleted stuff from gitignore

---
+++ app/controllers/AdminController.php
@@ -0,0 +1,15 @@
+<?php
+
+class AdminController extends BaseController {
+
+	public function __construct()
+	{
+		$this->beforeFilter('csrf_json', array('on' => 'post'));
+	}
+	
+	public function index()
+	{
+		return View::make('admin');
+	}
+
+}

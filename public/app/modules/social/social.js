7766ef60630945a022b2fb4ed681a961c66925c2
omfgitsjack <jackinyiu@gmail.com>
2014-07-13 22:00:33 -0400

    [Front-end] Modified Social List into a directive, can only extend the page length

--- public/app/modules/social/social.js
+++ public/app/modules/social/social.js
@@ -9,63 +9,86 @@ controllers.socialController = function($scope, $filter, UserSvc, authentication
 	// Services
 	$scope.UserSvc = UserSvc;
 
-	// Variables
-	var allUsers;
-	// Search models
-	$scope.asyncSelectedUser;
-	// Pagination models
-	$scope.pageLength;
-	$scope.currentPage;
 	$scope.totalItems;
-	
 
 	function init() {
 		// variables
-		$scope.pageLength = 7; // Pagination
-		$scope.currentPage = 1; // Pagination
 		$scope.UserSvc = UserSvc;
 
 		$scope.UserSvc.ListAll().then(
 			function(data){
 				allUsers = data;
-				$scope.users = allUsers; 
-				$scope.totalItems = $scope.users.length;
+				$scope.userlistz = allUsers; 
+				//$scope.totalItems = $scope.userlistz.length;
 			});
 	};
-	init();
-
-	/**
-	 * Updates pagination variables
-	 */
-	$scope.updateUsers = function(selectedUser) {
-		// Filter out the users
-		var filteredUsers = $filter("filter")(allUsers, {name:selectedUser});
-
-		// Update Pagination Variables
-		resetPagination(filteredUsers.length);
-	}
-
-	$scope.clear = function(selectedUser) {
-		if (selectedUser) {
-			$scope.asyncSelectedUser = "";
-			$scope.updateUsers($scope.asyncSelectedUser);
-		}
-	}
-
-	/**
-	 * Reset Pagination to page 1 & reset number of pages based on the
-	 * number of items
-	 */
-	var resetPagination = function(numberOfItems) {
-		$scope.currentPage = 1;
-		$scope.totalItems = numberOfItems;
-	}
 };
 
-directives.jpuserlist = function() {
+directives.jpuserlist = function($filter, UserSvc, authenticationService) {
 	return {
 		restrict: 'E',
-		templateUrl: 'app/modules/social/user-list.tmpl.html'
+		transclude: true,
+		templateUrl: 'app/modules/social/user-list.tmpl.html',
+		scope: {
+			pageLength: "@"
+			//userlist: "&userList"
+		},
+		link: function($scope, element, attrs) {
+			// Services
+			$scope.UserSvc = UserSvc;
+
+			// Variables
+			var allUsers;
+			// Search models
+			$scope.asyncSelectedUser;
+			// Pagination models
+			$scope.pageLength;
+			$scope.currentPage;
+			$scope.totalItems;
+
+			function init() {
+				// variables
+				$scope.currentPage = 1; // Pagination
+				$scope.UserSvc = UserSvc;
+				// Initialize variables from data attributes
+				
+				$scope.UserSvc.ListAll().then(
+					function(data){
+						allUsers = data;
+						$scope.users = allUsers; 
+						$scope.totalItems = $scope.users.length;
+						$scope.pageLength = attrs.pagelength;
+					});
+			};
+			init();
+
+			/**
+			 * Updates pagination variables
+			 */
+			$scope.updateUsers = function(selectedUser) {
+				// Filter out the users
+				var filteredUsers = $filter("filter")(allUsers, {name:selectedUser});
+
+				// Update Pagination Variables
+				resetPagination(filteredUsers.length);
+			}
+
+			$scope.clear = function(selectedUser) {
+				if (selectedUser) {
+					$scope.asyncSelectedUser = "";
+					$scope.updateUsers($scope.asyncSelectedUser);
+				}
+			}
+
+			/**
+			 * Reset Pagination to page 1 & reset number of pages based on the
+			 * number of items
+			 */
+			var resetPagination = function(numberOfItems) {
+				$scope.currentPage = 1;
+				$scope.totalItems = numberOfItems;
+			}
+		}
 	}
 };
 

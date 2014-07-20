fc507092c5f2c62499daacbd1aad19d4413462d0
omfgitsjack <jackinyiu@gmail.com>
2014-07-20 00:43:34 -0400

    [Front-end] [#95] Adding this directive reduces the flickering and is less flaky.

--- public/app/modules/app.js
+++ public/app/modules/app.js
@@ -97,3 +97,26 @@ app.run(['$rootScope', '$state', function($rootScope, $state){
 	    $rootScope.$broadcast('savestate');
 	};*/
 }]);
+
+app.directive('navCollapse', function () {
+    return {
+        restrict: 'A',
+        link: function (scope, element, attrs) {
+            var visible = false;
+
+            element.on('show.bs.collapse', function () {
+                visible = true;
+            });
+
+            element.on("hide.bs.collapse", function () {
+                visible = false;
+            });
+
+            element.on('click', function(event) {
+                if (visible && 'auto' == element.css('overflow-y')) {
+                    element.collapse('hide');
+                }
+            });
+        }
+    };
+});

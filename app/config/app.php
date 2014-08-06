3ff732e8216c49cd6b58a436644249a34651289b
yichen <yichenzhu1337@gmail.com>
2014-08-05 21:55:48 -0400

    fixed swagger php package and remodified configuration, localhost:8000/api-docs should work

--- app/config/app.php
+++ app/config/app.php
@@ -122,7 +122,8 @@ return array(
 		'Illuminate\View\ViewServiceProvider',
 		'Illuminate\Workbench\WorkbenchServiceProvider',
 		'Way\Generators\GeneratorsServiceProvider',
-		'Cartalyst\Sentry\SentryServiceProvider'
+		'Cartalyst\Sentry\SentryServiceProvider',
+		'Jlapp\Swaggervel\SwaggervelServiceProvider'
 	),
 
 	/*
@@ -189,8 +190,8 @@ return array(
 		'URL'             => 'Illuminate\Support\Facades\URL',
 		'Validator'       => 'Illuminate\Support\Facades\Validator',
 		'View'            => 'Illuminate\Support\Facades\View',
-		'Sentry' => 'Cartalyst\Sentry\Facades\Laravel\Sentry',
-		'Carbon' => 'Carbon\Carbon',
+		'Sentry' 		  => 'Cartalyst\Sentry\Facades\Laravel\Sentry',
+		'Carbon' 		  => 'Carbon\Carbon',
 	),
 
 );

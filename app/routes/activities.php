765851a21b3f14df5334f88c8276f8e980744c48
omfgitsjack <jackinyiu@gmail.com>
2014-08-23 21:52:49 -0400

    [Back-end] Moved the routing for activities/activities-all-this-week up

--- app/routes/activities.php
+++ app/routes/activities.php
@@ -22,6 +22,32 @@ use Swagger\Annotations as SWG;
 
 /**
  * @SWG\Api(
+ *   path="/activities/activities-all-this-week",
+ *   @SWG\Operation(
+ *     method="GET",
+ *     summary="Find an activity by activity_id",
+ *     notes="Returns an activity based on activity_id",
+ *     type="Activity",
+ *     nickname="getActivityById",
+ *     @SWG\Parameter(
+ *       name="activity_id",
+ *       description="id of activity that needs to be fetched",
+ *       required=true,
+ *       type="integer",
+ *       format="int64",
+ *       paramType="path",
+ *       minimum="1.0",
+ *       maximum="100000.0"
+ *     ),
+ *     @SWG\ResponseMessage(code=400, message="Invalid activity_id supplied"),
+ *     @SWG\ResponseMessage(code=404, message="Activity not found")
+ *   )
+ * )
+ */
+Route::get('activities/activities-all-this-week', 'ActivitiesController@getAllActivitiesThisWeek');
+
+/**
+ * @SWG\Api(
  *   path="/activities",
  *   @SWG\Operation(
  *     method="GET",
@@ -142,32 +168,6 @@ use Swagger\Annotations as SWG;
  */
 
 Route::resource('activities', 'ActivitiesController', ['only' => ['index', 'show', 'store', 'update','destroy']]);
-
-/**
- * @SWG\Api(
- *   path="/activities/activities-all-this-week",
- *   @SWG\Operation(
- *     method="GET",
- *     summary="Find an activity by activity_id",
- *     notes="Returns an activity based on activity_id",
- *     type="Activity",
- *     nickname="getActivityById",
- *     @SWG\Parameter(
- *       name="activity_id",
- *       description="id of activity that needs to be fetched",
- *       required=true,
- *       type="integer",
- *       format="int64",
- *       paramType="path",
- *       minimum="1.0",
- *       maximum="100000.0"
- *     ),
- *     @SWG\ResponseMessage(code=400, message="Invalid activity_id supplied"),
- *     @SWG\ResponseMessage(code=404, message="Activity not found")
- *   )
- * )
- */
-Route::get('activities/activities-all-this-week', 'ActivitiesController@getAllActivitiesThisWeek');
 
 /**
  * @SWG\Api(

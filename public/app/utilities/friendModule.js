716e7737e44f6e6f0a32931aa929a982c13c868a
omfgitsjack <jackinyiu@gmail.com>
2014-08-26 19:57:04 -0400

    [Front-end] [#182] Refactor naming of friendModule

--- public/app/utilities/friendModule.js
+++ public/app/utilities/friendModule.js
@@ -1,7 +1,6 @@
 var mod = angular.module('jp.friend', ['jp.friend.status.config','jp.api.config','jp.http']);
 var services= {};
 
-
 services.FriendService = function(friend_statuses, api_const, API) {
 	var base_friend_url = api_const.base_api_route+'/'+api_const.friends;
 	var base_notification_url = api_const.base_api_route+'/'+api_const.notifications;
@@ -31,7 +30,7 @@ services.FriendService = function(friend_statuses, api_const, API) {
 
 	this.deleteFriendRequest = function(curId, targetId)
 	{
-		
+		// Delete Notification?
 	};
 
 	this.unFriend = function(targetId)

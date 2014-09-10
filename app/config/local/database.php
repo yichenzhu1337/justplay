e21df3a807871ec711162d6e8b493feadeb0eaa3
yichen <yichenzhu1337@gmail.com>
2014-09-10 00:15:25 -0400

    [Backend] added db migration local configuration

--- app/config/local/database.php
+++ app/config/local/database.php
@@ -1,19 +1,52 @@
 <?php
 
-return array(
+    $env = "db"; //p - production, d - development, db - migrate db
 
-	'connections' => array(
+    if($env == "p")
+    {
+        $ENV =[
+            'DB_HOST' => getenv('DB_HOST'),
+            'DB_NAME' => getenv('DB_NAME'),
+            'DB_USERNAME' => getenv('DB_USERNAME'),
+            'DB_PASSWORD' => getenv('DB_PASSWORD')
+        ];
+    }
+    elseif ($env == "db")
+    {
+        $ENV =[
+            'DB_HOST' => '107.170.116.126',
+            'DB_NAME' => 'justplay',
+            'DB_USERNAME' => 'forge',
+            'DB_PASSWORD' => 'Khd9hJxJQSiDIFu40v9e'
+        ];
+    }
+    else
+    {
+        $ENV =[
+            'DB_HOST' => 'localhost',
+            'DB_NAME' => 'justplay',
+            'DB_USERNAME' => 'root',
+            'DB_PASSWORD' => 'root'
+        ];
+    }
 
-		'mysql' => array(
-			'driver'    => 'mysql',
-			'host'      => 'localhost',
-			'database'  => 'justplay',
-			'username'  => 'root',
-			'password'  => 'root',
-			'charset'   => 'utf8',
-			'collation' => 'utf8_unicode_ci',
-			'prefix'    => '',
-		)
+    return [
 
-	)
-);
+        'default' => 'mysql',
+
+        'connections' => [
+
+            'mysql' => [
+                'driver'    => 'mysql',
+                'host'      => $ENV['DB_HOST'],
+                'database'  => $ENV['DB_NAME'],
+                'username'  => $ENV['DB_USERNAME'],
+                'password'  => $ENV['DB_PASSWORD'],
+                'charset'   => 'utf8',
+                'collation' => 'utf8_unicode_ci',
+                'prefix'    => '',
+            ]
+
+        ],
+
+    ];

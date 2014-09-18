55286aa3cfdceef04cf1a0c1c8d222c7d92de8a7
omfgitsjack <jackinyiu@gmail.com>
2014-09-18 11:54:52 -0400

    [Testing] Tried updating base_route

--- public/app/modules/notifications/notificationsModule.js
+++ public/app/modules/notifications/notificationsModule.js
@@ -21,7 +21,7 @@ controllers.notificationsController = ['$scope', 'friendNotifications', 'activit
 }];
 
 factories.notificationsFactory = function(notification_routes, notification_categories, api_const, API, $filter, $state) {
-	var base_route = api_const.base_url+api_const.base_api_route+'/'+api_const.notifications;
+	var base_route = api_const.base_api_route+'/'+api_const.notifications;
 
 	/// HELPER FUNCTIONS
 	// Parses and formats the activityName

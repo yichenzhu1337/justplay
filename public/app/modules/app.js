4646730b673e5a6d07bab056f31f53c2f5f72ced
omfgitsjack <jackinyiu@gmail.com>
2014-09-28 10:52:01 -0400

    [#223] Added workaround for auto-filled passwords not picked up by angular

--- public/app/modules/app.js
+++ public/app/modules/app.js
@@ -376,3 +376,18 @@ app.directive('navCollapse', function() {
         }
     };
 });
+
+app.directive('autoFillSync', function($timeout) {
+   return {
+      require: 'ngModel',
+      link: function(scope, elem, attrs, ngModel) {
+          var origVal = elem.val();
+          $timeout(function () {
+              var newVal = elem.val();
+              if(ngModel.$pristine && origVal !== newVal) {
+                  ngModel.$setViewValue(newVal);
+              }
+          }, 500);
+      }
+   }
+});

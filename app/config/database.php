304276950bae4440862158683645c2080aaf4459
Jack Yiu <jackinyiu@gmail.com>
2015-05-07 13:16:17 -0400

    Update database.php

--- app/config/database.php
+++ app/config/database.php
@@ -54,10 +54,10 @@ return array(
 
 		'mysql' => array(
 			'driver'    => 'mysql',
-            'host'      => '104.236.229.36',
-            'database'  => 'forge',
-            'username'  => 'forge',
-            'password'  => 'ZVHydjdIJOZwZGlai54u',
+			'host'      => '45.55.244.193',
+			'database'  => 'forge',
+			'username'  => 'forge',
+			'password'  => 'Ct3ubmsoCQtE93IZuFSy',
 			'charset'   => 'utf8',
 			'collation' => 'utf8_unicode_ci',
 			'prefix'    => '',
